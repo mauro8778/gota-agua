ba73c453f903a0e0b7f61534d299afc841acc52d
MDiaz8778 <maurodiaz8778@gmail.com>
2024-10-19 14:21:08 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
